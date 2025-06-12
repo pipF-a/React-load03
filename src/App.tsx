48bcdcb6e91a784f868c67aad4b1cce530b249f6
pipF-a <pip87213@gmail.com>
2025-06-12 10:42:01 +0900

    リファクタ

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 
-// import { useState } from 'react';
 import './App.css'
 import { Header } from './components/Header/Header'
-// import { InputArea } from './components/InputArea'
 import { Memo } from './components/Memo/Memo'
 
 function App() {
-  // const [items, setItems] = useState<number[]>([]);
-  // const [newItemCount, setNewItemCount] = useState(1);
-
-
-
-  // /* テキストエリアのテキストを保持する関数*/
-  // const [textContent,setTextContent] = useState('');
-
-  // /* テキストが変更されたら実行する関数 */
-  // const handleChangeText = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   setTextContent(e.target.value)
-  // }
-
   return (
     <>
       <Header/>
